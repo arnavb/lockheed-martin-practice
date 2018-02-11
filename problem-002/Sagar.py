7289fc7f8a679bb6c599837ea62fe694b33c7388
Semicolon <sagarreddypatil@gmail.com>
2018-02-11 15:46:13 -0500

    idunno

---
+++ problem-002/Sagar.py
@@ -0,0 +1,14 @@
+with open("Prob02.in.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+content.pop(0)
+for line in content:
+    linearr = line.split()
+    letter_to_pop = int(linearr[1])
+    word = linearr[0]
+    del linearr
+    #pythons better
+    word = list(word)
+    word.pop(letter_to_pop)
+    word = ''.join(word)
+    print(word)
