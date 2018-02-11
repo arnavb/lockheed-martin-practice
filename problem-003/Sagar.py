1f35906466cc7200e21f1ff08d287559fe92b5da
Semicolon <sagarreddypatil@gmail.com>
2018-02-11 16:05:22 -0500

    bow to me b*****s

--- problem-003/Sagar.py
+++ problem-003/Sagar.py
@@ -2,4 +2,6 @@ with open("Prob03.in.txt") as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 content.pop(0)
-print(content)
+for thing in content:
+    thing = thing.split()
+    print(str(int(thing[0]) + int(thing[1])) + ' ' + str(int(thing[0]) * int(thing[1])))
