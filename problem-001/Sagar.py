92b76436bd8f013e1328189d87acb48b71df4465
Sagar Reddy Patil <sagarreddypatil@hotmail.com>
2018-02-11 15:25:44 -0500

    pushhhhhh

--- problem-001/Sagar.py
+++ problem-001/Sagar.py
@@ -2,5 +2,10 @@ with open("Prob01.in.txt") as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
+i = 0
+timeLoop = 0
 for line in content:
-    print("Kill me")
+    if(i != 0):
+        for i in range(0, 2):
+            print(line)
+    i = i + 1
